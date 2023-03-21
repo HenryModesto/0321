3c3734c6cf9f42fc7071a7d9044eb670293561e4
Henry Modesto <114154481+HenryModesto@users.noreply.github.com>
2023-03-21 12:20:00 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import { LinearGradient} from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image} from 'react-native';
+import InputEmail from './src/components/Input/InputEmail';
+import InputSenha from './src/components/Input/InputPassword';
+
+import { Ionicons } from '@expo/vector-icons'
+
+import estilo from './src/estilo';
+
+import Header from './src/components/header';
+
+import footer from './src/components/Footer';
+
+ export default function App() { 
+  return ( 
+       
+        <View style={estilo.container}>
+          
+
+        <Header />
+
+
+          { /* View com entrada para dados **/  }
+
+          <View style={estilo.box}>
+
+
+           <InputEmail />
+           <InputSenha />
+           
+
+            <TouchableOpacity style={{width: '80%'}} >
+              <LinearGradient
+              colors={['#d2691e', '#ff4500']}
+              style={estilo.button}>
+
+                <Text style={estilo.text}> Entrar </Text>
+
+              </LinearGradient>
+
+
+            </TouchableOpacity>
+
+          </View>  
+
+          <Footer/>
+
+        </View>
+  );
+}
